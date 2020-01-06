47620b6d9c0158355f85287bf53ab090c8a8d664
pk-42 <215796@edu.p.lodz.pl>
2020-01-06 21:09:19 +0100

    fied 1.5

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -5,23 +5,38 @@ import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
+/**
+ * klasa odpowiedzialna za zwracanie instancji klas z DefaultCountingOutRhymer
+ */
 public class DefaultRhymersFactory implements Rhymersfactory {
 
+    /**
+     * @return zwracana nowy obiekt klasy DefaultCountingOutRhymer
+     */
     @Override
     public DefaultCountingOutRhymer GetStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     * @return zwracana nowy obiekt klasy DefaultCountingOutRhymer
+     */
     @Override
     public DefaultCountingOutRhymer GetFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     * @return zwracana nowy obiekt klasy FIFORhymer
+     */
     @Override
     public DefaultCountingOutRhymer GetFIFORhymer() {
         return new FIFORhymer();
     }
 
+    /**
+     * @@return zwracana nowy obiekt klasy HanoiRhymer
+     */
     @Override
     public DefaultCountingOutRhymer GetHanoiRhymer() {
         return new HanoiRhymer();
--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,7 +1,5 @@
 package edu.kis.vh.nursery;
 
-import edu.kis.vh.nursery.DefaultCountingOutRhymer;
-import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
@@ -10,8 +8,15 @@ class RhymersDemo {
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
 
-        DefaultCountingOutRhymer[] rhymers = {factory.GetStandardRhymer(), factory.GetFalseRhymer(),
+        testRhymers(factory);
+
+    }
+
+
+    private static void testRhymers(Rhymersfactory factory) {
+        DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
                 factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
+
 
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
@@ -29,7 +34,6 @@ class RhymersDemo {
 
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
-
     }
 
 }
